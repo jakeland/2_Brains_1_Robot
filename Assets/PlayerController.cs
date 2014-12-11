dde4d8a959c199884a68f1c9d9e45a94f05b0942
jakeland <jakemc@embarqmail.com>
2014-12-11 13:05:52 -0500

    added control script, changed controls to better suit game play style, need to figure out how to rotate the arms correctly, need to fix arms being controlled by both players.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,56 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 	[HideInInspector]
 	public float playerRotation;
-
-
+	
+	
 	
 	public float moveForce = 365f;
 	public float maxSpeed = 5f;
-
 	
-
-
+	
+	
+	
 	private Animator anim;
 	
 	
 	void Awake ()
 	{
-
+		
 	}
 	
 	void Update ()
 	{
+		float arml = Input.GetAxis ("Fire3");
+		if (arml == 1)
+		{
 
+		}
 	}
 	
 	void FixedUpdate ()
 	{
-		
-				float h = Input.GetAxis ("Horizontal");
-				float j = Input.GetAxis ("Vertical");
-				//anim.SetFloat("Speed", Mathf.Abs(h));
-		
-				if (h * rigidbody2D.velocity.x < maxSpeed)
+
+		float h = Input.GetAxis ("Horizontal");
+		float j = Input.GetAxis ("Vertical");
+		//anim.SetFloat("Speed", Mathf.Abs(h));
+
+
+		if (h > -1 && h < 1) {
+						rigidbody2D.velocity = new Vector2 (0.0f, rigidbody2D.velocity.y);
+
+				}
+		if (j > -1 && j < 1){
+						rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 0.0f);
+
+		}
+		if (h * rigidbody2D.velocity.x < maxSpeed)
 			
-						rigidbody2D.AddForce (Vector2.right * h * moveForce);
+			rigidbody2D.AddForce (Vector2.right * h * moveForce);
 		
-				if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
+		if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
 			
-						rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
 		
-				if (j * rigidbody2D.velocity.y < maxSpeed)
-						rigidbody2D.AddForce (Vector2.right * j * moveForce);
-				
-				if (Mathf.Abs (rigidbody2D.velocity.y) > maxSpeed)
-						rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.y) * maxSpeed, rigidbody2D.velocity.y);
+		if (j * rigidbody2D.velocity.y < maxSpeed)
+			rigidbody2D.AddForce (Vector2.up * j * moveForce);
+		
+		if (Mathf.Abs (rigidbody2D.velocity.y) > maxSpeed)
+			rigidbody2D.velocity = new Vector2 ( rigidbody2D.velocity.x, Mathf.Sign (rigidbody2D.velocity.y) * maxSpeed);
 
 	}
-		
-		
+
+	
+	
 	
 	
 }
