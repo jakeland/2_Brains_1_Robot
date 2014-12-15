018e07ec2cd2bc85d482f9812b68abe30aa5ddb1
jakeland <jakemc@embarqmail.com>
2014-12-15 12:08:56 -0500

    added levels, fixed rotation of player, added rotating arms, and finished player. Set up level one set up scripts, and created player pre-fab. created manager object.

---
+++ Assets/Scripts/ArmScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmScript : MonoBehaviour {
+
+	public Transform armL;
+	public Transform armR; 
+	public float speed = 80f;
+
+	private float rtl = 0f; //rotation tracker left
+	private float rtr = 0f; //rotation tracker right
+
+	void FixedUpdate(){
+		 
+		float rml = 0f;//rotation modifier left
+
+		float rmr = 0f;//rotation modifier right
+
+
+
+		if (Input.GetButton ("ArmLB"))
+						rml = -1;
+	if (Input.GetButton ("ArmLF"))
+						rml = 1;
+
+	if (Input.GetButton ("ArmRF"))
+					rmr = -1;
+		if (Input.GetButton ("ArmRB"))
+						rmr = 1;
+
+
+
+		rtl += Time.deltaTime * rml * speed;
+		rtr += Time.deltaTime * rmr * speed;
+
+		if (rtl < 0)
+						rtl = 0;
+		if (rtr > 0)
+						rtr = 0;
+		if (rtl > 180)
+						rtl = 180;
+		if (rtr < -180)
+						rtr = -180;
+
+
+		print ("rtl = " + rtl);
+		print ("rtr = " + rtr);
+		if (((rtl + Time.deltaTime * rml) > 0) && ((rtl + Time.deltaTime * rtl) < 180)) {
+						armL.transform.Rotate  (0, 0, rml * Time.deltaTime * speed);
+			}
+		if (((rtr + Time.deltaTime * rmr) > -180) && ((rtr + Time.deltaTime * rtr) < 0)) {
+						armR.transform.Rotate (0, 0, rmr * Time.deltaTime * speed);
+				}
+
+
+
+	}
+	
+
+	
+
+
+	void Update(){
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	[HideInInspector]
+	public float playerRotation;
+	private string rotate = "";
+	
+	
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float rotationRate = 5f;
+
+	
+	
+	private Animator anim;
+	
+	
+	void Awake ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+
+	}
+	
+	void FixedUpdate ()
+	{
+
+		float h = Input.GetAxis ("Horizontal");
+		float j = Input.GetAxis ("Vertical");
+
+
+		//anim.SetFloat("Speed", Mathf.Abs(h));
+		if (Input.GetButton ("Left") && Input.GetButton ("Right")) {
+			h = 0;
+						
+						
+				} 
+		if (Input.GetButton ("Up") && Input.GetButton ("Down")) {
+						j = 0;
+				}
+						if (h ==-1f && j == 0) {
+								rotate = "w";
+						}
+						if (h == 1f && j == 0) {
+								rotate = "e";
+						}
+						if (h == 0 && j == 1f) {
+								rotate = "n";
+						}
+						if (h == 0 && j == -1f) {
+								rotate = "s";
+						}
+						if (h == 1f && j ==1f) {
+								rotate = "ne";
+						}
+						if (h == 1f && j == -1f) {
+								rotate = "se";
+						}
+						if (h == -1f && j == 1f) {
+								rotate = "nw";
+						}
+						if (h == -1 && j == -1f) {
+								rotate = "sw";
+						}
+
+						if (h > -1 && h < 1) {
+								rigidbody2D.velocity = new Vector2 (0.0f, rigidbody2D.velocity.y);
+								
+						}
+						if (j > -1 && j < 1) {
+								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 0.0f);
+								
+						}
+						if (h * rigidbody2D.velocity.x < maxSpeed)
+			
+								rigidbody2D.AddForce (Vector2.right * h * moveForce);
+		
+						if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
+			
+								rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		
+						if (j * rigidbody2D.velocity.y < maxSpeed)
+								rigidbody2D.AddForce (Vector2.up * j * moveForce);
+		
+						if (Mathf.Abs (rigidbody2D.velocity.y) > maxSpeed)
+								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, Mathf.Sign (rigidbody2D.velocity.y) * maxSpeed);
+						
+								checkRotate ();
+				
+	}
+
+	void checkRotate(){
+		float currZ = transform.rotation.eulerAngles.z;
+		float targetZ;
+		Quaternion toRot;
+		if (currZ < 0)
+						currZ = 360 + currZ;
+	switch (rotate) {
+	
+		case "n":
+			targetZ = 0f;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		case "s":
+			targetZ = 180f;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		case "e":
+			targetZ = 270f;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		case "w":
+			targetZ = 90f;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		case "ne":
+			targetZ = 315f;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+
+			break;
+		case "se":
+			targetZ = 225;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		case "sw":
+			targetZ = 135;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		case "nw":
+			targetZ = 45;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+		default:
+			targetZ = transform.eulerAngles.z;
+			toRot = Quaternion.Euler(0f,0f,targetZ);
+			break;
+				}
+		transform.rotation = Quaternion.Slerp (transform.rotation, toRot, Time.deltaTime * rotationRate);
+	}
+	
+}
